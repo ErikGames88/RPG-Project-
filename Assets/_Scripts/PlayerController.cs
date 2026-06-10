46780e2f3c5e02646696305b813bafc562077120
ErikGames88 <erik.pan35@gmail.com>
2026-06-10 12:12:18 +0200

    Player movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f; 
+    private const string Axis_H = "Horizontal", Axis_V = "Vertical";
 
     void Start()
     {
@@ -13,9 +14,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Space = Velocitiy * Time (S = V * T)
-        if(Input.GetAxisRaw("Horizontal") > 0.2f)
+        if(Mathf.Abs(Input.GetAxisRaw(Axis_H)) > 0.2f)
         {
-            Vector3 translation = new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f);
+            Vector3 translation = new Vector3(Input.GetAxisRaw(Axis_H) * speed * Time.deltaTime, 0f, 0f);
+            transform.Translate(translation);
+        }
+
+        if(Mathf.Abs(Input.GetAxisRaw(Axis_V)) > 0.2f)
+        {
+            Vector3 translation = new Vector3(0f, Input.GetAxisRaw(Axis_V) * speed * Time.deltaTime, 0f);
+            transform.Translate(translation);
         }
     }
 
